ebd994b3565deefb49584ea178513ed1b5593e33
andre.pratama <andreepratama27@gmail.com>
2023-08-16 09:25:44 +0700

    feat: add vitest for component testing

---
+++ src/lib/services/anime.service.ts
@@ -0,0 +1,42 @@
+/* eslint-disable no-useless-catch */
+import { ApiUrl } from "../api/constants";
+import { Anime, AnimeResponse } from "../api/type";
+
+export const fetchAnime = async (): Promise<AnimeResponse> => {
+  try {
+    const response = await fetch(`${ApiUrl}/anime?page=1&limit=10`, {
+      cache: "no-cache",
+    });
+    const result = await response.json();
+
+    return result;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const fetchAnimeDetail = async ({
+  id,
+}: {
+  id: number;
+}): Promise<Anime> => {
+  try {
+    const response = await fetch(`${ApiUrl}/anime/${id}`);
+    const result = await response.json();
+
+    return result;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const fetchAnimeByQuery = async ({ search }: { search: string }) => {
+  try {
+    const response = await fetch(`${ApiUrl}/anime?q=${search}`);
+    const result = await response.json();
+
+    return result;
+  } catch (error) {
+    throw error;
+  }
+};
